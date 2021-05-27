39c7765d8c68203076a8da5b42098319cdbd425b
Tomoko <haisaitomoco@gmail.com>
2021-05-27 14:20:06 +0300

    Add list component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { List } from "./List";
 
 function App() {
+  const [description, setDescription] = useState('View before Click');
+
+  const changeDescription = () => {
+    setDescription('view after click');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { description }
+      <List title="Can you see me?"/>
+      <button onClick={() => changeDescription() }> button </button>
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,11 @@
+export const List = ( { title }) => {
+    
+    return (
+        <div>
+            <h4>{ title }</h4>
+        
+
+            <div>This is List Component</div>
+        </div>
+    )
+}
